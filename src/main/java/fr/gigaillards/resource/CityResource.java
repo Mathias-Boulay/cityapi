a3c2d2f1db4d2c42028b8c4cc7702c013166d7cf
Martin Moreira de Jesus <martin.moreira-de-jesus@clicnwork.io>
2023-07-04 15:14:23 +0200

    fix: use micrometer instead of smallrye

--- src/main/java/fr/gigaillards/resource/CityResource.java
+++ src/main/java/fr/gigaillards/resource/CityResource.java
@@ -2,8 +2,11 @@ package fr.gigaillards.resource;
 
 import fr.gigaillards.entity.City;
 import fr.gigaillards.repository.CityRepository;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
-import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -11,9 +14,6 @@ import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.metrics.MetricUnits;
-import org.eclipse.microprofile.metrics.annotation.Counted;
-import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import java.util.List;
 
@@ -21,21 +21,20 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CityResource {
-
     @Inject
     CityRepository cityRepository;
 
     @GET
-    @Counted(name = "performedCalls", description = "How many calls have been made to this resource")
-    @Timed(name = "callTimer", description = "A measure of how long it takes to perform the call", unit = MetricUnits.MILLISECONDS)
+    @Timed(value = "city.all.time", description = "Time taken to delete a city")
+    @Counted(value = "city.all.count", description = "Amount of times the endpoint was called")
     public Uni<List<City>> findAll() {
         return cityRepository.findAll();
     }
 
     @POST
+    @Timed(value = "city.create.time", description = "Time taken to create a city")
+    @Counted(value = "city.create.count", description = "Amount of times the endpoint was called")
     @WithTransaction
-    @Counted(name = "performedCalls", description = "How many calls have been made to this resource")
-    @Timed(name = "callTimer", description = "A measure of how long it takes to perform the call", unit = MetricUnits.MILLISECONDS)
     public Uni<Response> createOne(@Valid City city) {
         return cityRepository.createCity(city);
     }
